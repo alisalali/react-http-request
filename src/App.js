c441ebf860ed95378921efab974a96987dff8b18
alisalali <alis.alali@hotmail.com>
2023-10-30 23:39:52 +0300

    async/await request

--- src/App.js
+++ src/App.js
@@ -5,22 +5,20 @@ import "./App.css";
 
 function App() {
   const [movies, setMovieList] = useState([]);
-  function fetchMovieHandler() {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const myMovieList = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        return setMovieList(myMovieList);
-      });
+
+  async function fetchMovieHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+
+    const transformMovies = await data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovieList(transformMovies);
   }
   return (
     <React.Fragment>
