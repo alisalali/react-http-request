7d6c6bb56bd103eb2d287d8dd28dbaca57a5eed6
alisalali <alis.alali@hotmail.com>
2023-10-31 00:47:15 +0300

    handling loading and data state

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import "./App.css";
 
 function App() {
   const [movies, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMovieHandler() {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
 
@@ -19,6 +21,7 @@ function App() {
       };
     });
     setMovieList(transformMovies);
+    setIsLoading(false);
   }
   return (
     <React.Fragment>
@@ -26,7 +29,9 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies</p>}
+        {isLoading && <p>loading</p>}
       </section>
     </React.Fragment>
   );
